0c7cd271aff64f28aaa98ab63efc4aaf2d05e7f0
John <rladygks02@gmail.com>
2025-11-29 13:13:31 -0800

    Make essential buttons functional/interactive
    Numeric buttons show on display when pressed,
    operator buttons correctly perform operations,
    equal button outputs result of expression,
    AC button clears the calculator to a clean slate

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const display = document.querySelector(".display");
+const numbers = document.querySelectorAll(".numeric");
+const operators = document.querySelectorAll(".operator");
+const equal = document.querySelector(".equal");
+const clearButton = document.querySelector(".clear");
+
+let firstNum = "";
+let secondNum = "";
+let operator = "";
+let result = "";
+let displayContent = "";
+let moreOperations = false;
+
 function addition(firstNum, secondNum) {
   return firstNum + secondNum;
 }
@@ -21,14 +34,58 @@ function operate(firstNum, operator, secondNum) {
   else if(operator === "-") {
     return subtraction(firstNum, secondNum);
   }
-  else if(operator === "*") {
+  else if(operator === "×") {
     return multiplication(firstNum, secondNum);
   }
-  else if(operator === "/") {
+  else if(operator === "÷") {
     return division(firstNum, secondNum);
   }
 }
 
-let firstNum = 0;
-let secondNum = 0;
-let operator = "+";
+function clear() {
+  firstNum = "";
+  secondNum = "";
+  operator = "";
+  result = "";
+  displayContent = "";
+  moreOperations = false;
+}
+
+numbers.forEach(element => {
+  element.addEventListener("click", () => {
+    displayContent += element.textContent;
+    display.textContent = displayContent;
+  })
+})
+
+operators.forEach(element => {
+  element.addEventListener("click", () => {
+    if(moreOperations === false) {
+      firstNum = parseInt(displayContent);
+      moreOperations = true;
+    }
+    else if(moreOperations === true) {
+      secondNum = parseInt(displayContent);
+      firstNum = operate(firstNum, operator, secondNum);
+      display.textContent = firstNum;
+    }
+
+    operator = element.textContent;
+    displayContent = "";
+
+    console.log("firstNum = " + firstNum);
+    console.log("operator = " + operator);
+  })
+})
+
+equal.addEventListener("click", () => {
+  secondNum = parseInt(displayContent);
+  result = operate(firstNum, operator, secondNum);
+  display.textContent = result;
+  clear();
+})
+
+clearButton.addEventListener("click", () => {
+  clear();
+  display.textContent = 0;
+})
